831184a01a47f54c5d9099c5140bef3d08ef698c
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-06 19:42:12 +0200

    page progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import Header from '../components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
 
 function MyApp({ Component, pageProps }) {
   return (
