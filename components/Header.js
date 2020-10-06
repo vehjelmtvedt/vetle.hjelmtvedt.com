330912b65db848421678b3894098a482d1501f29
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-06 22:27:50 +0200

    navbar is mobile friendly

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,19 @@ import styles from './navbar.module.css'
 export default function Header() {
     return (
         <>
-            <Navbar sticky="top" className="justify-content-between" id={styles.navbar}>
+            <Navbar collapseOnSelect expand="lg" sticky="top" id={styles.navbar}>
+
 
                 <Link href="/"><Navbar.Brand href="/">Vetle Hjelmtvedt</Navbar.Brand></Link>
 
-                <Nav className="mr-sm-2">
-                    <Link href="/"><Nav.Link href="/">Home</Nav.Link></Link>
-                    <Link href="/projects"><Nav.Link href="/projects">Projects</Nav.Link></Link>
-                    <Link href="/about"><Nav.Link href="/about">About</Nav.Link></Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav className={styles.navitems}>
+                        <Link href="/"><Nav.Link href="/">Home</Nav.Link></Link>
+                        <Link href="/projects"><Nav.Link href="/projects">Projects</Nav.Link></Link>
+                        <Link href="/about"><Nav.Link href="/about">About</Nav.Link></Link>
+                    </Nav>
+                </Navbar.Collapse>
 
             </Navbar>
         </>
