6798b618e3f79804933c9a69988150abdaf48ced
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-07 20:17:25 +0200

    navbar is mobile friendly

--- components/Header.js
+++ components/Header.js
@@ -13,14 +13,12 @@ export default function Header() {
         window.addEventListener("scroll", () => {
             setIsScrolled(window.scrollY > 100);
         });
-    }, []); 
-
-
+    }, []);
     
     return (
         <>
             <Navbar collapseOnSelect expand="md" fixed="top" id={styles.navbar} className={isScrolled ? styles.isScrolledSection : styles.notScrolledSection} style={{transition: '0.3s ease'}}>
-                <Link href="/"><Navbar.Brand href="/"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>Vetle Hjelmtvedt</p></Navbar.Brand></Link>
+                <Link href="/"><Navbar.Brand href="/"><p id={styles.navbrand} className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>Vetle Hjelmtvedt</p></Navbar.Brand></Link>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
