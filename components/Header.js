b37ee14b6c5807f15ab0ea301dcd12750247b371
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-07 19:45:58 +0200

    refactored for desktop version, mobile remains

--- components/Header.js
+++ components/Header.js
@@ -19,15 +19,15 @@ export default function Header() {
     
     return (
         <>
-            <Navbar collapseOnSelect expand="md" fixed="top" id={styles.navbar}>
-                <Link href="/"><Navbar.Brand href="/"><p id={styles.brandText}>Vetle Hjelmtvedt</p></Navbar.Brand></Link>
+            <Navbar collapseOnSelect expand="md" fixed="top" id={styles.navbar} className={isScrolled ? styles.isScrolledSection : styles.notScrolledSection} style={{transition: '0.3s ease'}}>
+                <Link href="/"><Navbar.Brand href="/"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>Vetle Hjelmtvedt</p></Navbar.Brand></Link>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav className={styles.navitems}>
-                        <Link href="/"><Nav.Link href="/"><p className={isScrolled ? styles.isScrolled : styles.notScrolled}>Home</p></Nav.Link></Link>
-                        <Link href="/projects"><Nav.Link href="/projects"><p className={styles.navHeading}>Projects</p></Nav.Link></Link>
-                        <Link href="/about"><Nav.Link href="/about"><p className={styles.navHeading}>About</p></Nav.Link></Link>
+                        <Link href="/"><Nav.Link href="/"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>Home</p></Nav.Link></Link>
+                        <Link href="/projects"><Nav.Link href="/projects"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText}  style={{transition: '0.3s ease'}}>Projects</p></Nav.Link></Link>
+                        <Link href="/about"><Nav.Link href="/about"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText}  style={{transition: '0.3s ease'}}>About</p></Nav.Link></Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
