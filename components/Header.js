d9b1dafb430066ca8299fc394f53d9905e646bc0
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-08 17:22:17 +0200

    color and underline on hover navbar

--- components/Header.js
+++ components/Header.js
@@ -18,12 +18,12 @@ export default function Header() {
     return (
         <>
             <Navbar collapseOnSelect expand="md" fixed="top" id={styles.navbar} className={isScrolled ? styles.isScrolledSection : styles.notScrolledSection} style={{transition: '0.3s ease'}}>
-                <Link href="/"><Navbar.Brand href="/"><p id={styles.navbrand} className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>V E T L E &nbsp; &nbsp; <span className={styles.secondName}>H J E L M T V E D T</span></p></Navbar.Brand></Link>
+                <Link href="/"><Navbar.Brand href="/"><p id={styles.navbrand} className={isScrolled ? styles.isScrolledBrand : styles.notScrolledBrand} style={{transition: '0.3s ease'}}>V E T L E &nbsp; &nbsp; <span className={styles.secondName}>H J E L M T V E D T</span></p></Navbar.Brand></Link>
 
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav className={styles.navitems}>
-                        <Link href="/"><Nav.Link href="/"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>H O M E</p></Nav.Link></Link>
+                        <Link href="/"><Nav.Link href="/"><p id={styles.testing} className={isScrolled ? styles.isScrolledText : styles.notScrolledText} style={{transition: '0.3s ease'}}>H O M E</p></Nav.Link></Link>
                         <Link href="/projects"><Nav.Link href="/projects"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText}  style={{transition: '0.3s ease'}}>P R O J E C T S</p></Nav.Link></Link>
                         <Link href="/about"><Nav.Link href="/about"><p className={isScrolled ? styles.isScrolledText : styles.notScrolledText}  style={{transition: '0.3s ease'}}>A B O U T</p></Nav.Link></Link>
                     </Nav>
