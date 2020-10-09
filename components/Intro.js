5d5486e8d68c4da5f6ab4ee6fac050687dbc3dec
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-09 15:05:26 +0200

    changed intro text

--- components/Intro.js
+++ components/Intro.js
@@ -1,6 +1,7 @@
 import styles from './intro.module.css'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import Aboutme from './Aboutme'
@@ -24,7 +25,9 @@ export default function Intro() {
                     <Container className={styles.container} style={{display: 'flex', justifyContent: 'center'}}>
                         <div className={styles.textdiv}>
                           <h1 className={styles.heading}>VETLE <span className={styles.secondName}>HJELMTVEDT</span></h1>
-                          <p className={styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam cursus dui eget ante hendrerit efficitur. Quisque libero nulla, porttitor ac luctus ac, tempus ut augue. Donec mollis ut odio eget consequat. Vestibulum ante ipsum primis in faucibus orci luctus et</p>
+                          <p className={styles.p}>Hi! I'm a computer science stuent at the University of Bergen and this is 
+                          my personal website containing my projects and contact information.</p>
+                          <Image src="/avatar.png" className={styles.image} roundedCircle />
                         </div>
                     </Container>
                     <div className={styles.div} onClick={executeScroll}>
