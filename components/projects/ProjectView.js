e2b50c2661c4786e2d8af0de2a4fdc089d8345e0
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-10 12:05:22 +0200

    add links to every project page

--- components/projects/ProjectView.js
+++ components/projects/ProjectView.js
@@ -1,5 +1,6 @@
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import styles from './projectview.module.css'
+import Link from 'next/link'
 import Container from 'react-bootstrap/Container'
 import { CardDeck, Card, Button } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -22,7 +23,7 @@ export default function ProjectView() {
                                     Server and client communicates through websockets. Once you join you will be placed in 
                                     a random lobby to play with others
                                 </p>
-                                <Button className={styles.button}>Read more <FontAwesomeIcon icon="glasses" /></Button>
+                                <Button className={styles.button}><Link className={styles.link} href="/projects/Ludo">Read more</Link> &nbsp; <FontAwesomeIcon icon="glasses" /></Button>
                                 <Button className={styles.button}><a className={styles.link} href="https://github.com/vehjelmtvedt/online-ludo.git">See on github </a> <FontAwesomeIcon icon={["fab", "github"]} /></Button>
                                 <Button className={styles.button}>Live demo <FontAwesomeIcon icon={faLongArrowAltRight} /></Button>
                             </Card>
@@ -37,7 +38,7 @@ export default function ProjectView() {
                                 <p className={styles.p}>A p5.js project aiming to visualize the most commonly-used sorting algorithms. 
                                  I have currently implemented Bubble and Selection sort. This is a work in progress and I am planning to publish this soon to a website.
                                   </p>
-                                <Button className={styles.button}>Read more <FontAwesomeIcon icon="glasses" /></Button>
+                                <Button className={styles.button}><Link className={styles.link} href="/projects/Visualgo">Read more </Link> &nbsp; <FontAwesomeIcon icon="glasses" /></Button>
                                 <Button className={styles.button}><a className={styles.link} href="https://github.com/vehjelmtvedt/visualgo.git">See on github </a> <FontAwesomeIcon icon={["fab", "github"]} /></Button>
                                 <Button className={styles.button}>Live demo <FontAwesomeIcon icon={faLongArrowAltRight} /></Button>
                             </Card>
@@ -52,7 +53,7 @@ export default function ProjectView() {
                                 <p className={styles.p}>A GUI application + server written in Java. Made as a group assignment in the first year 
                                 of university. It functions as a social media in which you can log activities that saves CO2 emissions and
                                 compare your emission savings with friends.</p>
-                                <Button className={styles.button}>Read more <FontAwesomeIcon icon="glasses" /></Button>
+                                <Button className={styles.button}><Link className={styles.link} href="/projects/GoGreen">Read more </Link> &nbsp; <FontAwesomeIcon icon="glasses" /></Button>
                                 <Button className={styles.button}><a className={styles.link} href="https://github.com/vehjelmtvedt/GoGreen.git">See on github </a><FontAwesomeIcon icon={["fab", "github"]} /></Button>
                                 <Button className={styles.button}>Live demo <FontAwesomeIcon icon={faLongArrowAltRight} /></Button>
                             </Card>
