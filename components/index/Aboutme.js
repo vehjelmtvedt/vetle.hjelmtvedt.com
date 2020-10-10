32cb28540a57c8558ecb719c02e86a70241d6fd4
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-10 11:08:59 +0200

    finished about me page

--- components/index/Aboutme.js
+++ components/index/Aboutme.js
@@ -6,28 +6,29 @@ export default function Aboutme() {
         <>
             <Jumbotron fluid className={styles.jumbotron}>
                 <h1 className={styles.heading}>A B O U T  &nbsp;  <span className={styles.secondName}>M E</span></h1>
+                <p className={styles.subheading}>I’m a computer science student at the University of Bergen, which means I am fortunate enough to study that which is also my hobby.  
+                </p>
                 <div className={styles.textcontainer}>
+                    
                     <div className={styles.text}>
-                        <p>I’m a computer science student at the University of Bergen, which means I am fortunate enough to study that which is also my hobby.  
-                        </p>
-                    </div>
-                    <div className={styles.text}>
-                        <h2>Web development</h2>
+                        <h3>Web development</h3>
                         <p>I mostly work with full stack web development using the node.js framework, although I prefer working with the backend components. This website is built using the next.js framework for React. Next.js pre-renders pages at build-time (SSG), which means lightning fast loading time, try it out yourself!
                         </p>
                     </div>
                     <div className={styles.text}>
-                        <h2>Java and Python</h2>
+                        <h3>Java and Python</h3>
                         <p>I’ve also completed various projects in Java such as the GUI application GoGreen. Similar to web development, I prefer working the backend of these applications. In addition, I am quite proficient in Python.
                         </p>
                     </div>
+                </div>
+                <div className={styles.textcontainer2}>
                     <div className={styles.text}>
-                        <h2>IOT</h2>
+                        <h3>IOT</h3>
                         <p>Along with my love of web development is IOT. By far, my coolest project must be my homebuilt Magic Mirror (pictures can be found on the projects page). It utilises a node.js application, which is highly modularised and as such allows a huge community to contribute to this project. You can check out my own module on the projects page.
                         </p>
                     </div>
                     <div className={styles.text}>
-                        <h2>Future ventures and projects</h2>
+                        <h3>Future ventures and projects</h3>
                         <ul>
                             <li>Docker and kubernetes</li>
                             <li>Deploy each project in docker and run it on my own server</li>
