b23d4b734bb37e5859e4d00f957b8e54e315d41d
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2020-10-10 12:18:04 +0200

    deleted projectcards section, not in use anymore

--- pages/projects/GoGreen.js
+++ pages/projects/GoGreen.js
@@ -1,7 +1,15 @@
+import Header from '../../components/global/Header'
+import Footer from '../../components/global/Footer'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
 export default function GoGreen() {
     return (
         <>
-            <h1>This is the GoGreen Project page.</h1>
+            <Header threshold={0}/>
+            <Jumbotron fluid>
+
+            </Jumbotron>
+            <Footer />
         </>
     )
 }
